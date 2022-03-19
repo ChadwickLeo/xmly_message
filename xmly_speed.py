77b2883a3ea1e1094b5358c86ac6f23cf4184823
ChadwickLeo <34240557+ChadwickLeo@users.noreply.github.com>
2022-03-19 08:10:04 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -224,8 +224,8 @@ def ans_getTimes(cookies):
     try:
         response = requests.get(
             'https://m.ximalaya.com/speed/web-earn/topic/user', headers=headers, cookies=cookies)
-        print(response.text)
         result = json.loads(response.text)
+        print("ans_getTimes: ", result)
         stamina = result["data"]["stamina"]
         remainingTimes = result["data"]["remainingTimes"]
         return {"stamina": stamina,
@@ -246,9 +246,9 @@ def ans_start(cookies):
     try:
         response = requests.get(
             'https://m.ximalaya.com/speed/web-earn/topic/start', headers=headers, cookies=cookies)
-        print("ans_start: ", response.text)
         result = json.loads(response.text)
-        if not (result and "data" in result and "paperId" in result["data"] and "dateStr" in result["data"] and "topics" in result["data"] and len(result["data"]["topics"])>2 and "topicId" in result["data"]["topics"][2]): return None
+        print("ans_start: ", result)
+        if not (result and "data" in result and "paperId" in result["data"] and "dateStr" in result["data"] and "topics" in result["data"] and type(result["data"]["topics"]) is list and len(result["data"]["topics"])>2 and "topicId" in result["data"]["topics"][2]): return None
         paperId = result["data"]["paperId"]
         dateStr = result["data"]["dateStr"]
         lastTopicId = result["data"]["topics"][2]["topicId"]
@@ -308,7 +308,7 @@ def lottery_info(cookies):
             return
         response = requests.get(
             'https://m.ximalaya.com/speed/web-earn/inspire/lottery/token', headers=headers, cookies=cookies)
-        print("token", response.text)
+        print("token: ", response.json())
         token = response.json()["data"]["id"]
         data = {
             "token": token,
@@ -576,9 +576,10 @@ def cardReportTime(cookies):
     try:
         response = requests.post('https://m.ximalaya.com/speed/web-earn/card/reportTime',
                                  headers=headers, cookies=cookies, data=json.dumps(data)).json()
-        print("cardReportTime: ", response.text)
-        if response["data"]["upperLimit"]:
-            print("今日已达上限")
+        if response:
+            print("cardReportTime: ", response.text)
+            if response["data"]["upperLimit"]:
+                print("今日已达上限")
     except:
         print(traceback.format_exc())
 
